26a2d572df444d32d2ac01854158b0947b9b2941
Marius <mawiit15@hs-esslingen.de>
2025-07-14 14:16:37 +0200

    .

--- backend/app.py
+++ backend/app.py
@@ -12,23 +12,23 @@ CORS(app)
 def search():
     query = request.args.get("q", "")
     result = indexing.search(query)
-    dummy_results = [
-        {
-            "title": f"Ergebnis 1 zu '{query}'",
-            "url": "https://www.tuebingen.de/",
-            "snippet": "Dies ist ein Beispiel-Snippet für die Suchmaschine TüBing."
-        },
-        {
-            "title": f"Ergebnis 2 zu '{query}'",
-            "url": "https://www.tuebingen.de/tourismus",
-            "snippet": "Noch ein weiteres tolles Suchergebnis zur Stadt Tübingen."
-        },
-        {
-            "title": "Schloss Hohentübingen",
-            "url": "https://www.schloss-tuebingen.de/",
-            "snippet": "Besuchen Sie das Schloss Hohentübingen und genießen Sie die Aussicht."
-        }
-    ]
+    # dummy_results = [
+    #     {
+    #         "title": f"Ergebnis 1 zu '{query}'",
+    #         "url": "https://www.tuebingen.de/",
+    #         "snippet": "Dies ist ein Beispiel-Snippet für die Suchmaschine TüBing."
+    #     },
+    #     {
+    #         "title": f"Ergebnis 2 zu '{query}'",
+    #         "url": "https://www.tuebingen.de/tourismus",
+    #         "snippet": "Noch ein weiteres tolles Suchergebnis zur Stadt Tübingen."
+    #     },
+    #     {
+    #         "title": "Schloss Hohentübingen",
+    #         "url": "https://www.schloss-tuebingen.de/",
+    #         "snippet": "Besuchen Sie das Schloss Hohentübingen und genießen Sie die Aussicht."
+    #     }
+    # ]
     return jsonify(result)
 
 @app.route("/api/start-crawling", methods=["GET","POST"])
